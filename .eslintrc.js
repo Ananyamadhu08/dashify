f368dce6dbea8ef9cfa3fb7bc3bcc26ba8e06ec1
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-04-08 12:41:49 +0530

    feat: created stacked chart component

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,7 @@ module.exports = {
         'jsx-a11y/no-static-element-interactions': ['off'],
         'jsx-a11y/click-events-have-key-events': ['off'],
         'no-template-curly-in-string': ['off'],
+        'react/no-array-index-key': ['off'],
+        'react/jsx-props-no-spreading': ['off'],
     },
 };
