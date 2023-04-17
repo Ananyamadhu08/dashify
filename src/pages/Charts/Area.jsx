acb47ccd11e1fe526ebcd62399b5a26d682a8de1
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-04-18 05:00:41 +0530

    fix: fixed titles

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -23,7 +23,7 @@ const Area = () => {
         <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
             <ChartsHeader
                 category="Area"
-                title="Inflation Rate in percentage"
+                title="Inflation Rate In Percentage"
             />
             <div className="w-full">
                 <ChartComponent
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -23,7 +23,7 @@ const Bar = () => {
 
     return (
         <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-            <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+            <ChartsHeader category="Bar" title="Rio Olympic Medal Counts" />
             <div className=" w-full">
                 <ChartComponent
                     id="charts"
